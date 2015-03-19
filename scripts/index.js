353c23b144a553076e5c2426d8d0cd3023088666
Pierre <pierre.malgorn@sfr.fr>
2015-03-19 10:57:09 +0100

    Ajout des trucs de baaaaasseee

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,38 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var AngJS = angular.module('AngJS', []);
 
-
+AngJS.controller('MainCtrl', function($scope, $http){
+    $scope.search = "Bidule";
+    $scope.users = [];
+    
+    $scope.selectUser = function(toto) {
+        $scope.currentUser = toto;
+    }
+    
+    $scope.ordering = {
+        'field' : 'name',
+        'reverse' : false
+    }
+    
+    $scope.selectOrderingField = function(column) {
+        if($scope.ordering.field === column) {
+            $scope.ordering.reverse = !$scope.ordering.reverse;
+        } else {
+            $scope.ordering.field = column;
+            $scope.ordering.reverse = false;
+        }
+        $scope.ordering.field = column;
+    }
+    
+    $http.get('data/data.json')
+            .success(function(data, status){
+                $scope.users = data;
+                $scope.status = status;
+                console.info(status);
+                console.log($scope.users);
+            })
+            .error(function(data, status){
+                $scope.status = status;
+                console.error(status);
+            });
+    
+});
