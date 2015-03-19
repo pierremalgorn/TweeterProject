85fc985ed9a35f1d1952def79444492d8d8031e4
Pierre <pierre.malgorn@sfr.fr>
2015-03-19 14:29:32 +0100

    Few modsssss

--- scripts/MainCtrl.js
+++ scripts/MainCtrl.js
@@ -1,23 +1,9 @@
 AngJS.controller('MainCtrl', function($scope, $http){
     $scope.tweets = [];
-    $scope.currentTweet;
-    
-    $scope.selectUser = function(toto) {
-        $scope.currentUser = toto;
-    }
-    
-//    $scope.selectOrderingField = function(column) {
-  //      if($scope.ordering.field === column) {
-    //        $scope.ordering.reverse = !$scope.ordering.reverse;
-      //  } else {
-        //    $scope.ordering.field = column;
-          //  $scope.ordering.reverse = false;
-        //}
-       // $scope.ordering.field = column;
-    //}
+    $scope.currentTweet = null;
     
     $scope.sendTweet = function() {
-        $http.post('http://54.93.114.212/tweets', {authorEmail: currentTweet.email, authorName: currentTweet.name, message: currentTweet.message})
+        $http.post('http://54.93.114.212/tweets', {authorEmail: $scope.currentTweet.email, authorName: currentTweet.name, message: currentTweet.message})
             .success(function(data){
                  $scope.tweets = data;
         })
