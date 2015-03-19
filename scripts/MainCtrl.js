1a76687f403efb6f20eecd0457ae099da8658d03
Pierre <pierre.malgorn@sfr.fr>
2015-03-19 15:15:15 +0100

    Ca chemar

--- scripts/MainCtrl.js
+++ scripts/MainCtrl.js
@@ -1,9 +1,13 @@
-AngJS.controller('MainCtrl', function($scope, $http){
+AngJS.controller('MainCtrl', function($scope, $http, $timeout){
     $scope.tweets = [];
     $scope.currentTweet = null;
     
+    $scope.getImgLink = function (hash) {
+        return 'http://www.gravatar.com/avatar/'+hash;
+    }
+    
     $scope.sendTweet = function() {
-        $http.post('http://54.93.114.212/tweets', {authorEmail: $scope.currentTweet.email, authorName: currentTweet.name, message: currentTweet.message})
+        $http.post('http://54.93.114.212/tweets', {authorEmail: $scope.currentTweet.email, authorName: $scope.currentTweet.name, message: $scope.currentTweet.message})
             .success(function(data){
                  $scope.tweets = data;
         })
@@ -11,14 +15,21 @@ AngJS.controller('MainCtrl', function($scope, $http){
                  $scope.tweets = data;
         })
     }
-
-
-    $http.get('http://54.93.114.212/tweets')
-        .success(function(data){
-             $scope.tweets = data;
-    })
-        .error(function(data){
-             $scope.tweets = data;
-    })
+    
+    var refreshMessages = function(){
+        $timeout(function(){
+            $http.get('http://54.93.114.212/tweets')
+                .success(function(data){
+                     $scope.tweets = data;
+            })
+                .error(function(data){
+                     $scope.tweets = data;
+            })
+            
+            refreshMessages();
+        }, 3000);        
+    };
+    
+    refreshMessages();
 
 });
